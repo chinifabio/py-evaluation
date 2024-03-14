e9742ebb242c6c321c7b461ed8c307ebef00c309
Fabio Chini <fabiochini99@gmail.com>
2024-03-14 13:17:27 +0100

    Update benchmarking scripts for row and column operations

--- benchmark_all.sh
+++ benchmark_all.sh
@@ -10,76 +10,130 @@ if [ ! -d "mprof_results" ]; then
     mkdir mprof_results
 fi
 
-hyperfine --warmup 3 --export-json hyperfine_results/groupby_mean.json \
+hyperfine --warmup 3 --export-json hyperfine_results/groupby_mean_row.json \
     --parameter-list n_row 1000,10000,100000,1000000,10000000 \
-    --parameter-list n_col 10,100 \
     --parameter-list lib pandas,polars,noir \
     --ignore-failure \
-    "./tests/{lib}/groupby_mean.py data/{n_row}_{n_col}.csv"
+    "./tests/{lib}/groupby_mean.py data/{n_row}_10.csv"
 
-hyperfine --warmup 3 --export-json hyperfine_results/join.json \
+hyperfine --warmup 3 --export-json hyperfine_results/join_row.json \
     --parameter-list n_row 1000,10000,100000,1000000,10000000 \
-    --parameter-list n_col 10,100 \
     --parameter-list lib pandas,polars,noir \
     --ignore-failure \
-    "./tests/{lib}/join.py data/{n_row}_{n_col}.csv data/1000_10.csv {n_col}"
+    "./tests/{lib}/join.py data/{n_row}_10.csv data/1000_10.csv 10"
 
-hyperfine --warmup 3 --export-json hyperfine_results/filter.json \
+hyperfine --warmup 3 --export-json hyperfine_results/filter_row.json \
     --parameter-list n_row 1000,10000,100000,1000000,10000000 \
-    --parameter-list n_col 10,100 \
     --parameter-list lib pandas,polars,noir \
     --ignore-failure \
-    "./tests/{lib}/filter.py data/{n_row}_{n_col}.csv"
+    "./tests/{lib}/filter.py data/{n_row}_10.csv"
 
-hyperfine --warmup 3 --export-json hyperfine_results/filter_agg.json \
+hyperfine --warmup 3 --export-json hyperfine_results/filter_agg_row.json \
     --parameter-list n_row 1000,10000,100000,1000000,10000000 \
-    --parameter-list n_col 10,100 \
     --parameter-list lib pandas,polars,noir \
     --ignore-failure \
-    "./tests/{lib}/filter_agg.py data/{n_row}_{n_col}.csv"
+    "./tests/{lib}/filter_agg.py data/{n_row}_10.csv"
 
+hyperfine --warmup 3 --export-json hyperfine_results/groupby_mean_col.json \
+    --parameter-list n_col 10,100,1000 \
+    --parameter-list lib pandas,polars,noir \
+    --ignore-failure \
+    "./tests/{lib}/groupby_mean.py data/100000_{n_col}.csv"
+
+hyperfine --warmup 3 --export-json hyperfine_results/join_col.json \
+    --parameter-list n_col 10,100,1000 \
+    --parameter-list lib pandas,polars,noir \
+    --ignore-failure \
+    "./tests/{lib}/join.py data/100000_{n_col}.csv data/1000_10.csv {n_col}"
+
+hyperfine --warmup 3 --export-json hyperfine_results/filter_col.json \
+    --parameter-list n_col 10,100,1000 \
+    --parameter-list lib pandas,polars,noir \
+    --ignore-failure \
+    "./tests/{lib}/filter.py data/100000_{n_col}.csv"
+
+hyperfine --warmup 3 --export-json hyperfine_results/filter_agg_col.json \
+    --parameter-list n_col 10,100,1000 \
+    --parameter-list lib pandas,polars,noir \
+    --ignore-failure \
+    "./tests/{lib}/filter_agg.py data/100000_{n_col}.csv"
+
+n_col=10
 for lib in 'noir' 'polars' 'pandas'; do
     for n_row in 10000 100000 1000000 10000000; do
-        for n_col in 10 100; do
-            echo "Benchmarking $lib with $n_row rows and $n_col columns"
-            mprof run -T 0.01 -o mprof_results/filter_${lib}_${n_row}_${n_col}.dat ./tests/$lib/filter.py data/${n_row}_${n_col}.csv
-            mprof run -T 0.01 -o mprof_results/filteragg_${lib}_${n_row}_${n_col}.dat ./tests/$lib/filter_agg.py data/${n_row}_${n_col}.csv
-            mprof run -T 0.01 -o mprof_results/groupby_${lib}_${n_row}_${n_col}.dat ./tests/$lib/groupby_mean.py data/${n_row}_${n_col}.csv
-            mprof run -T 0.01 -o mprof_results/join_${lib}_${n_row}_${n_col}.dat ./tests/$lib/join.py data/${n_row}_${n_col}.csv data/1000_10.csv $n_col
-        done
+        echo "Benchmarking $lib with $n_row rows and 10 columns"
+        mprof run -T 0.01 -o mprof_results/filter_${lib}_${n_row}_${n_col}_row.dat ./tests/$lib/filter.py data/${n_row}_${n_col}.csv
+        mprof run -T 0.01 -o mprof_results/filteragg_${lib}_${n_row}_${n_col}_row.dat ./tests/$lib/filter_agg.py data/${n_row}_${n_col}.csv
+        mprof run -T 0.01 -o mprof_results/groupby_${lib}_${n_row}_${n_col}_row.dat ./tests/$lib/groupby_mean.py data/${n_row}_${n_col}.csv
+        mprof run -T 0.01 -o mprof_results/join_${lib}_${n_row}_${n_col}_row.dat ./tests/$lib/join.py data/${n_row}_${n_col}.csv data/1000_10.csv ${n_col}
+    done
+done
+
+n_row=100000
+for lib in 'noir' 'polars' 'pandas'; do
+    for n_row in 10000 100000 1000000 10000000; do
+        echo "Benchmarking $lib with $n_row rows and 10 columns"
+        mprof run -T 0.01 -o mprof_results/filter_${lib}_${n_row}_${n_col}_col.dat ./tests/$lib/filter.py data/${n_row}_${n_col}.csv
+        mprof run -T 0.01 -o mprof_results/filteragg_${lib}_${n_row}_${n_col}_col.dat ./tests/$lib/filter_agg.py data/${n_row}_${n_col}.csv
+        mprof run -T 0.01 -o mprof_results/groupby_${lib}_${n_row}_${n_col}_col.dat ./tests/$lib/groupby_mean.py data/${n_row}_${n_col}.csv
+        mprof run -T 0.01 -o mprof_results/join_${lib}_${n_row}_${n_col}_col.dat ./tests/$lib/join.py data/${n_row}_${n_col}.csv data/1000_10.csv ${n_col}
     done
 done
 
 PWD = $(pwd) # current directory, PyNoir needs absolute paths
 
-hyperfine --warmup 3 --export-json hyperfine_results/distributed_groupby_mean.json \
+hyperfine --warmup 3 --export-json hyperfine_results/distributed_groupby_mean_row.json \
     --ignore-failure \
     --parameter-list n_row 1000,10000,100000,1000000,10000000 \
-    --parameter-list n_col 10,100 \
     --parameter-list n_hosts 2,3,4 \
     "${PWD}/tests/noir/distributed_groupby_mean.py -r ${PWD}/{n_hosts}_hosts.yml \
         ${PWD}/data/{n_row}_10.csv"
 
-hyperfine --warmup 3 --export-json hyperfine_results/distributed_join.json \
+hyperfine --warmup 3 --export-json hyperfine_results/distributed_join_row.json \
     --ignore-failure \
     --parameter-list n_row 1000,10000,100000,1000000,10000000 \
-    --parameter-list n_col 10,100 \
     --parameter-list n_hosts 2,3,4 \
     "${PWD}/tests/noir/distributed_join.py -r ${PWD}/{n_hosts}_hosts.yml \
-        ${PWD}/data/{n_row}_10.csv ${PWD}/data/1000_10.csv {n_col}"
+        ${PWD}/data/{n_row}_10.csv ${PWD}/data/1000_10.csv 10"
 
-hyperfine --warmup 3 --export-json hyperfine_results/distributed_filter.json \
+hyperfine --warmup 3 --export-json hyperfine_results/distributed_filter_row.json \
     --ignore-failure \
     --parameter-list n_row 1000,10000,100000,1000000,10000000 \
-    --parameter-list n_col 10,100 \
     --parameter-list n_hosts 2,3,4 \
     "${PWD}/tests/noir/distributed_filter.py -r ${PWD}/{n_hosts}_hosts.yml \
         ${PWD}/data/{n_row}_10.csv"
 
-hyperfine --warmup 3 --export-json hyperfine_results/distributed_filter_agg.json \
+hyperfine --warmup 3 --export-json hyperfine_results/distributed_filter_agg_row.json \
     --ignore-failure \
     --parameter-list n_row 1000,10000,100000,1000000,10000000 \
-    --parameter-list n_col 10,100 \
     --parameter-list n_hosts 2,3,4 \
     "${PWD}/tests/noir/distributed_filter_agg.py -r ${PWD}/{n_hosts}_hosts.yml \
         ${PWD}/data/{n_row}_10.csv"
+
+hyperfine --warmup 3 --export-json hyperfine_results/distributed_groupby_mean_col.json \
+    --ignore-failure \
+    --parameter-list n_col 10,100,1000 \
+    --parameter-list n_hosts 2,3,4 \
+    "${PWD}/tests/noir/distributed_groupby_mean.py -r ${PWD}/{n_hosts}_hosts.yml \
+        ${PWD}/data/100000_{n_col}.csv"
+
+hyperfine --warmup 3 --export-json hyperfine_results/distributed_join_col.json \
+    --ignore-failure \
+    --parameter-list n_col 10,100,1000 \
+    --parameter-list n_hosts 2,3,4 \
+    "${PWD}/tests/noir/distributed_join.py -r ${PWD}/{n_hosts}_hosts.yml \
+        ${PWD}/data/100000_{n_col}.csv ${PWD}/data/1000_10.csv {n_col}"
+
+hyperfine --warmup 3 --export-json hyperfine_results/distributed_filter_col.json \
+    --ignore-failure \
+    --parameter-list n_col 10,100,1000 \
+    --parameter-list n_hosts 2,3,4 \
+    "${PWD}/tests/noir/distributed_filter.py -r ${PWD}/{n_hosts}_hosts.yml \
+        ${PWD}/data/100000_{n_col}.csv"
+
+hyperfine --warmup 3 --export-json hyperfine_results/distributed_filter_agg_col.json \
+    --ignore-failure \
+    --parameter-list n_col 10,100,1000 \
+    --parameter-list n_hosts 2,3,4 \
+    "${PWD}/tests/noir/distributed_filter_agg.py -r ${PWD}/{n_hosts}_hosts.yml \
+        ${PWD}/data/100000_{n_col}.csv"
