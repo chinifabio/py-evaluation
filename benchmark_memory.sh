8171ec57324e52c8078c9611beead27beb9598e5
Fabio Chini <fabiochini99@gmail.com>
2024-03-15 11:59:08 +0100

    Add benchmark_memory.sh script for memory profiling

---
+++ benchmark_memory.sh
@@ -0,0 +1,21 @@
+n_col=10
+for lib in 'noir' 'polars' 'pandas'; do
+    for n_row in 10000 100000 1000000 10000000; do
+        echo "Benchmarking $lib with $n_row rows and 10 columns"
+        mprof run -T 0.01 -o mprof_results/filter_${lib}_${n_row}_${n_col}_row.dat ./tests/$lib/filter.py data/${n_row}_${n_col}.csv
+        mprof run -T 0.01 -o mprof_results/filteragg_${lib}_${n_row}_${n_col}_row.dat ./tests/$lib/filter_agg.py data/${n_row}_${n_col}.csv
+        mprof run -T 0.01 -o mprof_results/groupby_${lib}_${n_row}_${n_col}_row.dat ./tests/$lib/groupby_mean.py data/${n_row}_${n_col}.csv
+        mprof run -T 0.01 -o mprof_results/join_${lib}_${n_row}_${n_col}_row.dat ./tests/$lib/join.py data/${n_row}_${n_col}.csv data/1000_10.csv ${n_col}
+    done
+done
+
+n_row=100000
+for lib in 'noir' 'polars' 'pandas'; do
+    for n_row in 10000 100000 1000000 10000000; do
+        echo "Benchmarking $lib with $n_row rows and 10 columns"
+        mprof run -T 0.01 -o mprof_results/filter_${lib}_${n_row}_${n_col}_col.dat ./tests/$lib/filter.py data/${n_row}_${n_col}.csv
+        mprof run -T 0.01 -o mprof_results/filteragg_${lib}_${n_row}_${n_col}_col.dat ./tests/$lib/filter_agg.py data/${n_row}_${n_col}.csv
+        mprof run -T 0.01 -o mprof_results/groupby_${lib}_${n_row}_${n_col}_col.dat ./tests/$lib/groupby_mean.py data/${n_row}_${n_col}.csv
+        mprof run -T 0.01 -o mprof_results/join_${lib}_${n_row}_${n_col}_col.dat ./tests/$lib/join.py data/${n_row}_${n_col}.csv data/1000_10.csv ${n_col}
+    done
+done
